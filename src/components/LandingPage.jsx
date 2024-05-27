36829fa15e0c397739d304cc0fa8a0539d7872d4
Tirth Patel <tirthp0112@gmail.com>
2024-05-26 21:07:22 -0500

    sds

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
         <div className='flex flex-col md:flex-row items-center md:space-x-16'>
           <div className='flex-1 text-center md:text-left'>
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
-              Dendritic Learning tirth
+              Dendritic Learning 
             </h1>
             <p className='md:text-2xl text-xl font-bold text-gray-500'>
               Revolutionize the way you learn
